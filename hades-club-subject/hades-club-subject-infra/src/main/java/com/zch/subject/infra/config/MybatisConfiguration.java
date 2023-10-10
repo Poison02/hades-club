e6aa48f5cd04d03a0d99389449ad76505b6c665f
zchhhhh <2069820192@qq.com>
2023-10-10 12:38:08 +0800

    feat：新增SQL拦截器翻译，可以在控制台打印完整的SQL

---
+++ hades-club-subject/hades-club-subject-infra/src/main/java/com/zch/subject/infra/config/MybatisConfiguration.java
@@ -0,0 +1,21 @@
+package com.zch.subject.infra.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Zch
+ * @date 2023/10/10
+ **/
+@Configuration
+public class MybatisConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new MybatisPlusAllSqlLog());
+        return mybatisPlusInterceptor;
+    }
+
+}
